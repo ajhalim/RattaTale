606c5797c96e252bd4e7e4f707a7f58c8e554a1e
mli254 <mli254@ucsc.edu>
2023-11-29 01:35:22 -0800

    moved UI elements to one canvas

--- Assets/scripts/playScript.cs
+++ Assets/scripts/playScript.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class playScript : MonoBehaviour
 {
-    public int adasfdas;
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
