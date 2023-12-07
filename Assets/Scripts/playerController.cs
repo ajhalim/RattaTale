9a4ac56634cf9f208483bdc42b6ce2f9ae8d59d3
LeCodingOwl <nguyenjimmy627@gmail.com>
2023-12-07 14:17:29 -0800

    Add rotation and a face to the rat. Camera updated
    I added eyes and nose to the rat so we know where it's facing. I also updated the camera to follow the player.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -10,7 +10,7 @@ public class playerController : MonoBehaviour
     private float gameTimer = 0f;
     public float moveSpeed = 5f;
     [SerializeField]
-    public float rotation;
+    public float rotation = 180f;
 
     public string currentIngredient = ""; 
 
@@ -42,11 +42,23 @@ public class playerController : MonoBehaviour
                     ingredient.transform.position = newPosition;
                 }
         }
+
+        //Calculate rotation based on input
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            rotation = Mathf.Atan2(verticalInput, horizontalInput) * Mathf.Rad2Deg;
+
+            rotation = Mathf.Round(rotation / 90) * 90;
+        }
     }
 
     private void FixedUpdate()
     {
         player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        player.MoveRotation(rotation);
     }
 
     string FormatTime(float timeInSeconds)
