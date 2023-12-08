11cf47e9e98313d69994765e8add7a3552352b8f
ajhalim <thenubbum@gmail.com>
2023-12-08 04:44:41 -0800

    LAST ONE I SWEAR

--- Assets/scripts/playScript.cs
+++ Assets/scripts/playScript.cs
@@ -7,6 +7,9 @@ public class playScript : MonoBehaviour
 {
     public void ChangeScene(string sceneName)
     {
+        PlayerPrefs.SetFloat("playTime", 0);
+        PlayerPrefs.SetFloat("lastRun", 90);
+        PlayerPrefs.SetInt("numRuns", 0);
         SceneManager.LoadScene(sceneName);
     }
     public void ReloadScene()
