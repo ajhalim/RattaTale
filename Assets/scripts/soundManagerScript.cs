0423bfa1c21fbc3762eebc72c96e8a2027001585
ajhalim <thenubbum@gmail.com>
2023-12-07 21:22:30 -0800

    our game is now actually

--- Assets/scripts/soundManagerScript.cs
+++ Assets/scripts/soundManagerScript.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class soundManagerScript : MonoBehaviour
 {
+    public AudioSource audioSource;
+    //public Slider audioSlider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource.volume = PlayerPrefs.GetFloat("GameVolume");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        audioSource.volume = PlayerPrefs.GetFloat("GameVolume");
     }
 }
